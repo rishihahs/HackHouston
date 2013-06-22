052762f524cb3cc8bdaf3a91b8576bab94aeba69
Rishi Shah <rishihahs@gmail.com>
2013-06-22 13:09:24 -0500

    Storage should work now

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,9 +1,9 @@
-/*$.getScript(window.city_config['api url'] + '/get/all?hash=' + hashData() + '&callback=jsonpCallback', function(){
+$.getScript(window.city_config['api url'] + '/get/all?hash=' + hashData() + '&callback=jsonpCallback', function(){
   $('#fees').html(''); // clear old data
   _Handler.removeAll(); // clear old handlers
   handler.initAll();
   window.fee_filter = create_filter(master_fees);
-});*/
+});
 
 function jsonpCallback(object) {
   if (!object) {
